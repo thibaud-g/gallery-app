76b4863598cd0e4d0563d71ed3d3deb29a0a1989
thibaud-g <thibaud.gippet@gmail.com>
2024-04-18 01:11:26 +0200

    fancy upload button

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -4,23 +4,18 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { UploadButton } from "~/utils/uploadthing";
-
+import { SimpleUploadButton } from "./simple-upload-button";
 const Navbar = () => {
   const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b border-white p-4">
       <p className="text-2xl font-semibold">Gallery App</p>
-      <div className="flex flex-row">
+      <div className="flex flex-row items-center gap-6">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
-            onClientUploadComplete={() => {
-              router.refresh();
-            }}
-            endpoint="imageUploader"
-          />
+          <SimpleUploadButton />
           <UserButton />
         </SignedIn>
       </div>
