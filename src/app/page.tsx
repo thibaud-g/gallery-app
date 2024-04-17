ba8d987f9a32c701a25b9252dbf045ca2fd62223
thibaud-g <thibaud.gippet@gmail.com>
2024-04-17 18:07:17 +0200

    change to schema and dispaly real data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,22 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const mockUrl = [
-    "https://utfs.io/f/71e2035b-9d7b-48b5-9011-968015b148f0-a48833.png",
-    "https://utfs.io/f/46b41882-096c-476d-9c5f-00aa543035e4-a4qp6e.png",
-    "https://utfs.io/f/f9742223-3548-42c5-8784-3ae64d50f1e3-lfg35a.png",
-    "https://utfs.io/f/b564ddbd-6df0-4189-8540-fda8d0a26a05-9jv45q.png",
-  ];
-
-  const mockImages = mockUrl.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
-
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.createdAt),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <img
-            key={image.id + "-" + index}
-            src={image.url}
-            className="w-64"
-            alt="mock"
-          />
+        {images.map((image) => (
+          <div key={image.id}>
+            <img
+              src={image.url}
+              className="aspect-video w-64 object-cover"
+              alt="mock"
+            />
+            <p>{image.name}</p>
+          </div>
         ))}
       </div>
     </main>
